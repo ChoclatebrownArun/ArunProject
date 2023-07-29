e3345ea45e3a47c278fde150ac809cf69c2df634
ChoclatebrownArun <pcarun143143@gmail.com>
2023-07-29 06:20:38 -0700

    first add by A branch

--- src/org/demo/GitDemo.java
+++ src/org/demo/GitDemo.java
@@ -3,7 +3,7 @@ package org.demo;
 public class GitDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Git class starts today...");
 
 	}
 
